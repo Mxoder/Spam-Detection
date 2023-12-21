a452fcc325a298b66fa7bfd2e0c78a60e387a0aa
Mxode <max_zhang2020@outlook.com>
2023-12-22 03:26:24 +0800

    Add config.py

--- src/config.py
+++ src/config.py
@@ -1,13 +1,13 @@
 import os
 
 
-cur_dir = os.path.dirname(os.path.abspath(__file__))                        # project/src
-par_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))       # project
+cur_dir = os.path.dirname(os.path.abspath(__file__))                    # project/src
+par_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))   # project
 
 # data
-data_dir = os.path.join(par_dir, 'data')
+data_dir = os.path.join(par_dir, 'data')                                # project/data
 data_train_path = os.path.join(data_dir, 'train.csv')
 data_test_path = os.path.join(data_dir, 'test.csv')
 
 # model
-model_dir = os.path.join(par_dir, 'model')
+model_dir = os.path.join(par_dir, 'model')                              # project/model
